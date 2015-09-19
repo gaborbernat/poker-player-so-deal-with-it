e762a5e2d6a892b4c99d86ebef1c9b5c807fba74
Gabor Bernat <gabor.bernat@gravityrd.com>
2015-09-19 09:54:21 +0200

    all in

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        return 0
+        return 1000
 
     def showdown(self, game_state):
         pass
