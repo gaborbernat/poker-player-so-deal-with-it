2f803c070ca8f5a134040122d334c828048f6f24
Adam Schmideg <adam.schmideg@prezi.com>
2015-09-19 10:00:28 +0200

    empty

--- player.py
+++ player.py
@@ -3,6 +3,7 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        # nothing special
         return 1000
 
     def showdown(self, game_state):
