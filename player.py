34bdb6665fa0d97831d4e6ac9d5a35f885fcf9e7
Adam Schmideg <adam.schmideg@prezi.com>
2015-09-19 11:35:25 +0200

    fix percentage

--- player.py
+++ player.py
@@ -18,7 +18,8 @@ class Player:
 
     def should_we_fold(self, hand):
         our_hand = self.order_cards([hand[0]['rank'], hand[1]['rank']])
-        return tuple(our_hand) not in hands
+        percentage = hands[tuple(our_hand)]
+        return percentage > 10
 
     def order_cards(self, elements):
         return sorted(elements, key=lambda x: -1 * self.order.index(x))
